d1aa12590a5ea286227a732e8e0e49bfd329dd50
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-06-01 14:37:10 +0530

    project setup done

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Hero } from "@/components";
+
+export default function Home() {
+  return (
+    <main className="">
+      <Hero />
+    </main>
+  );
+}
---
+++ src/components/hero.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const hero = () => {
+  return <main className="h-screen bg-blue-300 w-full">Hero 1</main>;
+};
+
+export default hero;
